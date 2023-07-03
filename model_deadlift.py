38b5f48230cb8b5994e266e92efbc65aff51d27d
Juan Roncancio <109776558+Juicibox@users.noreply.github.com>
2023-07-03 11:10:28 -0500

    Update model_deadlift.py

--- model_deadlift.py
+++ model_deadlift.py
@@ -11,46 +11,6 @@ mp_pose = mp.solutions.pose
 pose = mp_pose.Pose(min_tracking_confidence=0.6, min_detection_confidence=0.6)
 
 
-class SessionState:
-    def __init__(self):
-        self.current_stage = ''
-        self.counter = 0
-        self.bodylang_prob = np.array([0, 0])
-        self.bodylang_class = ''
-        self.body_language = ''
-
-
-# new instance SessionState
-session_state = SessionState()
-
-def detect(frame):
-
-    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = pose.process(image)
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(106, 13, 173), thickness=4, circle_radius=5),
-                              mp_drawing.DrawingSpec(color=(255, 102, 0), thickness=5, circle_radius=10))
-    try:
-        row = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten().tolist()
-        X = pd.DataFrame([row], columns = landmarks)
-        session_state.bodylang_prob  = model.predict_proba(X)[0]
-        session_state.bodylang_class = model.predict(X)[0]
-        session_state.body_language = model1.predict(X)[0]
-
-        if session_state.bodylang_class== "down" and session_state.bodylang_prob[session_state.bodylang_prob.argmax()] > 0.7:
-            session_state.current_stage = "down"
-        elif session_state.current_stage == "down" and session_state.bodylang_class == "up" and session_state.bodylang_prob[session_state.bodylang_prob.argmax()] > 0.7:
-            session_state.current_stage = "up"
-            session_state.counter += 1
-
-    except Exception as e:
-        print(e)
-
-    return image
-def reset_counter():
-
-    session_state.counter = 0
-
 
 def main():
     st.set_page_config(page_title="Peso Muerto", page_icon="💪")
